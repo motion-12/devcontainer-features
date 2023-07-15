a57f316a843239f7db6217a69096bce75629a058
Meegan1 <meegan118@gmail.com>
2023-07-15 12:37:26 +0000

    fix: remove unused command from gcloud

--- src/gcloud/install.sh
+++ src/gcloud/install.sh
@@ -9,5 +9,3 @@ source $(dirname $0)/get-user.sh
 apt-get update
 bash $(dirname $0)/install-gcloud.sh
 apt-get autoremove -y && apt-get clean -y && rm -rf /var/lib/apt/lists/* /tmp/library-scripts/
-
-echo $USERNAME >>/tmp/username.txt
